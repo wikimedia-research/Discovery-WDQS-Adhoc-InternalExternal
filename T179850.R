37c135f7411e02ed1da04dc8495d14979f0b8138
Mikhail Popov <mpopov@wikimedia.org>
2017-11-14 14:41:47 -0800

    SPARQL query execution time plots

--- T179850.R
+++ T179850.R
@@ -57,3 +57,35 @@ requests %>%
     caption = "In general we get quite a bit of external traffic from bots to the SPARQL endpoint.
     These requests were split according to the current \"bot\" detection."
   )
+
+sparql <- readr::read_csv("T179850v2.csv") %>%
+  dplyr::arrange(date, traffic)
+
+ggplot(sparql, aes(x = date, y = total_ttfb)) +
+  geom_bar(stat = "identity") +
+  facet_grid(agent_type ~ traffic, scales = "free_y") +
+  scale_y_continuous(labels = function(x) {
+    return(gsub(" 0s", "", tolower(lubridate::seconds_to_period(x)), fixed = TRUE))
+  }) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b '%y") +
+  wmf::theme_facet(14, "Open Sans", strip.text.y = element_text(angle = 0)) +
+  labs(
+    x = "Date", y = "Total time to first byte (s)",
+    title = "SPARQL query execution time by traffic and (basic) agent type",
+    subtitle = "Internal means the IP address started with a \"10.\""
+  )
+
+ggplot(sparql, aes(x = date, y = total_ttfb)) +
+  geom_bar(stat = "identity") +
+  facet_grid(agent_type_plus ~ traffic, scales = "free_y") +
+  scale_y_continuous(labels = function(x) {
+    return(gsub(" 0s", "", tolower(lubridate::seconds_to_period(x)), fixed = TRUE))
+  }) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b '%y") +
+  wmf::theme_facet(14, "Open Sans", strip.text.y = element_text(angle = 0)) +
+  labs(
+    x = "Date", y = "Total time to first byte (s)",
+    title = "SPARQL query execution time by traffic and (extended) agent type",
+    subtitle = "Internal means the IP address started with a \"10.\"",
+    caption = "Extended spider detection includes UAs with GitHub URLs (for example)."
+  )
