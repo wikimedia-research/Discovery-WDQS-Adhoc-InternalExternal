a71f545e3b18566bfb9e0d4844023c8f2998446c
Mikhail Popov <mpopov@wikimedia.org>
2017-11-14 16:56:28 -0800

    Reportization :)

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
-# Wikidata Query Service Traffic Analysis
+This shows a breakdown of internal vs external WDQS usage for
+Phabricator task [T179850](https://phabricator.wikimedia.org/T179850).
 
-This shows a breakdown of internal vs external WDQS usage for Phabricator task [T179850](https://phabricator.wikimedia.org/T179850).
+Per-second traffic/usage
+------------------------
+
+![Per-second count of requests made to SPARQL & LDF endpoint during
+October 2017, broken down by internal vs external
+traffic.](figures/per_sec_requests-1.png)
+
+Most of the time there are fewer than 100 internal requests per second
+to the SPARQL endpoint. External usage of SPARQL endpoint varies
+considerably, with as many as 500 reqs/sec observed during one hour on
+October 14th.
+
+Daily traffic/usage
+-------------------
+
+In general we get quite a bit of external traffic from bots to the
+SPARQL endpoint:
+
+### Requests
+
+![](figures/by_traffic_agent_destination-1.png)
+
+![](figures/by_traffic_agent_destination-2.png)
+
+![](figures/by_traffic_agent_destination-3.png)
+
+### Time to first byte (TTFB)
+
+![](figures/ttfb-1.png)
+
+![](figures/ttfb-2.png)
--- per_second.R
+++ per_second.R
@@ -49,25 +49,4 @@ results <- results[order(results$ts, results$traffic, results$destination), ]
 readr::write_csv(results, "wdqs-oct17-per-sec.csv")
 
 # Locally:
-library(magrittr)
-library(ggplot2)
-
 # system("scp stat5:/home/bearloga/wdqs-oct17-per-sec.csv ./")
-requests <- readr::read_csv("wdqs-oct17-per-sec.csv") %>%
-  dplyr::rename(`99th percentile` = percentile99) %>%
-  tidyr::gather("summary", "requests", -c(ts, traffic, destination)) %>%
-  tidyr::spread(summary, requests, fill = 0) %>%
-  tidyr::gather("per-hour summary", "requests", -c(ts, traffic, destination))
-
-ggplot(requests, aes(x = ts, y = requests, color = `per-hour summary`)) +
-  geom_line() +
-  scale_color_brewer(palette = "Set1") +
-  facet_grid(destination ~ traffic, scales = "free_y") +
-  wmf::theme_facet(14, "Open Sans", strip.text.y = element_text(angle = 0)) +
-  labs(
-    x = "Date", y = "Requests per second",
-    title = "WDQS per-second internal vs external SPARQL & LDF endpoint usage during Oct 2017",
-    subtitle = "Internal means the IP address started with a \"10.\"; aggregates were made on an hourly basis",
-    caption = "Most of the time there are fewer than 100 internal requests per second to the SPARQL endpoint.
-    External usage of SPARQL endpoint varies considerably, with as many as 500 reqs/sec observed during one hour on October 14th."
-  )
